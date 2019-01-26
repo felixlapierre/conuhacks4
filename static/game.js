977e5d9b37d0c14f3271afd64b30cda5b03aa5dd
arejayelle <35614138+arejayelle@users.noreply.github.com>
2019-01-26 15:55:09 -0500

    sending keyinput events to backend and added player file

--- static/game.js
+++ static/game.js
@@ -1,3 +1,47 @@
 var socket = io();
 
 socket.emit('new connection');
+var movement = {
+	up: false,
+	down: false,
+	left: false,
+	right: false
+}
+
+//Keyboard event listeners
+document.addEventListener('keydown', function(event) {
+	switch(event.keyCode) {
+		case 65: //A
+		movement.left = true;
+		break;
+		case 87: //W
+		movement.up = true;
+		break;
+		case 68: //D
+		movement.right = true;
+		break;
+		case 83: //S
+		movement.down = true;
+		break;
+	}
+});
+
+document.addEventListener('keyup', function(event) {
+	switch(event.keyCode) {
+		case 65: //A
+		movement.left = false;
+		break;
+		case 87: //W
+		movement.up = false;
+		break;
+		case 68: //D
+		movement.right = false;
+		break;
+		case 83: //S
+		movement.down = false;
+		break;
+	}
+});
+
+var map=undefined.
+socket.emit('new connection');
